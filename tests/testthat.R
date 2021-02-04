1865ef6ff863c280a9e4dacef2f2ed75fef73a82
maltelueken <malte_lueken@arcor.de>
2021-02-04 10:49:35 +0100

    Add workaround for installing Prophet from source on unix

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,9 @@
+if (Sys.getenv("GITHUB_ACTIONS") == "true" && .Platform$OS.type == "unix") {
+  install.packages("prophet", type = "source")
+} else { # just to make sure which branch we're going into
+  print("not installing prophet from source")
+}
+
 library(jaspTools)
 library(testthat)
 
