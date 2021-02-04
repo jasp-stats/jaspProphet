fda3bc8970d8b311e6c462d9c044b7c99df54352
maltelueken <malte_lueken@arcor.de>
2021-02-04 13:01:49 +0100

    Change installation from source for unix

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,6 @@
 if (Sys.getenv("GITHUB_ACTIONS") == "true" && .Platform$OS.type == "unix") {
-  install.packages("prophet", type = "source")
+  install.packages("prophet", type = "source",
+                   lib = "/Users/runner/work/jaspProphet/pkgs/Frameworks/R.framework/Versions/3.6/Resources/library")
 } else { # just to make sure which branch we're going into
   print("not installing prophet from source")
 }
