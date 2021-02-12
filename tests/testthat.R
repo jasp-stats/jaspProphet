8756d5cd6ece01e6b99bf96128477fc454d9a946
maltelueken <malte_lueken@arcor.de>
2021-02-12 09:37:24 +0100

    Revert installing prophet from source for unix

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,3 @@
-if (Sys.getenv("GITHUB_ACTIONS") == "true" && .Platform$OS.type == "unix") {
-  install.packages("prophet", type = "source",
-                   lib = "/Users/runner/work/jaspProphet/pkgs/Frameworks/R.framework/Versions/3.6/Resources/library")
-} else { # just to make sure which branch we're going into
-  print("not installing prophet from source")
-}
 
 library(jaspTools)
 library(testthat)
